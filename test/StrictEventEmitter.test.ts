ff4f8524fdc8b5c77949ff81530ba7f209db1848
Artem Zakharchenko <kettanaito@gmail.com>
2022-09-27 21:08:11 +0200

    test: add a "once" unit test

--- test/StrictEventEmitter.test.ts
+++ test/StrictEventEmitter.test.ts
@@ -16,3 +16,17 @@ test('restricts on/emit methods to the given events map', () => {
 
   emitter.removeAllListeners()
 })
+
+test('dispatches "once" callback only once', () => {
+  const callback = jest.fn()
+  const emitter = new StrictEventEmitter<EventsMap>()
+
+  emitter.once('ping', callback)
+
+  emitter.emit('ping', 5)
+  emitter.emit('ping', 10)
+  emitter.emit('ping', 15)
+
+  expect(callback).toBeCalledTimes(1)
+  expect(callback).toBeCalledWith(5)
+})
