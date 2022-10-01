7e206b2c347fef8a44c9bb77f256dfe2bac67f22
Artem Zakharchenko <kettanaito@gmail.com>
2022-10-01 13:16:42 +0200

    fix: removes all listeners on "removeAllListeners"

--- test/StrictEventEmitter.test.ts
+++ test/StrictEventEmitter.test.ts
@@ -5,7 +5,7 @@ interface EventsMap {
   pong: (n: number) => void
 }
 
-test('restricts on/emit methods to the given events map', () => {
+it('restricts on/emit methods to the given events map', () => {
   const callback = jest.fn()
   const emitter = new StrictEventEmitter<EventsMap>()
   emitter.on('ping', callback)
@@ -17,7 +17,7 @@ test('restricts on/emit methods to the given events map', () => {
   emitter.removeAllListeners()
 })
 
-test('dispatches "once" callback only once', () => {
+it('dispatches "once" callback only once', () => {
   const callback = jest.fn()
   const emitter = new StrictEventEmitter<EventsMap>()
 
@@ -29,4 +29,33 @@ test('dispatches "once" callback only once', () => {
 
   expect(callback).toBeCalledTimes(1)
   expect(callback).toBeCalledWith(5)
+})
+
+describe('removeAllListeners()', () => {
+  it('removes all listeners when called without any arguments', () => {
+    const callback = jest.fn()
+    const emitter = new StrictEventEmitter<EventsMap>()
+
+    emitter.on('ping', callback)
+    emitter.removeAllListeners()
+
+    emitter.emit('ping', 5)
+    expect(callback).not.toHaveBeenCalled()
+  })
+
+  it('removes all listeners for a specific event', () => {
+    const pingListener = jest.fn()
+    const pongListener = jest.fn()
+    const emitter = new StrictEventEmitter<EventsMap>()
+
+    emitter.on('ping', pingListener)
+    emitter.on('pong', pongListener)
+    emitter.removeAllListeners('ping')
+
+    emitter.emit('ping', 5)
+    expect(pingListener).not.toHaveBeenCalled()
+
+    emitter.emit('pong', 5)
+    expect(pongListener).toHaveBeenCalledWith(5)
+  })
 })
